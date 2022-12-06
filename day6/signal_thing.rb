8642216e4320200ca32f12132eebd74fc2e0f644
Ryan Brushett <ryanbrushett@gmail.com>
2022-12-06 00:45:08 -0500

    overengineer it, why not

--- day6/signal_thing.rb
+++ day6/signal_thing.rb
@@ -1,72 +1,64 @@
+require 'minitest/autorun'
+
 class SignalThing
-  require 'minitest/autorun'
+  attr_reader :input_data, :signal_length, :packet
 
-  def run(data, problem_part:)
-    case problem_part
-    when "one"
-      signal_length = 4
-    when "two"
-      signal_length = 14
-    else
-      signal_length = "wat"
-    end
+  def initialize(input_data:, signal_length:)
+    @input_data = input_data
+    @signal_length = signal_length
+    @packet = []
+  end
 
-    chars = []
-    char_count = 0
-    data.each_char.with_index do |x, i|
-      if chars.length < signal_length
-        chars << x
+  def run
+    input_data.each_char.with_index do |x, i|
+      if packet.length < signal_length
+        packet << x
         next
       end
-      return i if chars & chars == chars
-      (chars << x).shift
+      return i if packet & packet == packet
+
+      (packet << x).shift
     end
   end
 
   class SignalThingTest < Minitest::Test
     def test_the_test_data_for_part_one
       test_cases = [
-        {data: "mjqjpqmgbljsphdztnvjfqwrcgsmlb", expected: 7},
-        {data: "bvwbjplbgvbhsrlpgdmjqwftvncz", expected: 5},
-        {data: "nppdvjthqldpwncqszvftbrmjlhg", expected: 6},
-        {data: "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", expected: 10},
-        {data: "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", expected: 11},
+        { data: 'mjqjpqmgbljsphdztnvjfqwrcgsmlb', expected: 7 },
+        { data: 'bvwbjplbgvbhsrlpgdmjqwftvncz', expected: 5 },
+        { data: 'nppdvjthqldpwncqszvftbrmjlhg', expected: 6 },
+        { data: 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', expected: 10 },
+        { data: 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', expected: 11 },
       ]
 
       test_cases.each do |test_case|
-        assert_equal(
-          test_case[:expected],
-          SignalThing.new.run(test_case[:data], problem_part: "one"),
-          "#{test_case[:data]} was wrong...",
-        )
+        signal_thing = SignalThing.new(input_data: test_case[:data], signal_length: 4)
+        assert_equal(test_case[:expected], signal_thing.run, "#{test_case[:data]} was wrong...")
       end
     end
 
     def test_the_test_data_for_part_two
       test_cases = [
-        {data: "mjqjpqmgbljsphdztnvjfqwrcgsmlb", expected: 19},
-        {data: "bvwbjplbgvbhsrlpgdmjqwftvncz", expected: 23},
-        {data: "nppdvjthqldpwncqszvftbrmjlhg", expected: 23},
-        {data: "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", expected: 29},
-        {data: "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", expected: 26},
+        { data: 'mjqjpqmgbljsphdztnvjfqwrcgsmlb', expected: 19 },
+        { data: 'bvwbjplbgvbhsrlpgdmjqwftvncz', expected: 23 },
+        { data: 'nppdvjthqldpwncqszvftbrmjlhg', expected: 23 },
+        { data: 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', expected: 29 },
+        { data: 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', expected: 26 },
       ]
 
       test_cases.each do |test_case|
-        assert_equal(
-          test_case[:expected],
-          SignalThing.new.run(test_case[:data], problem_part: "two"),
-          "#{test_case[:data]} was wrong...",
-        )
+        signal_thing = SignalThing.new(input_data: test_case[:data], signal_length: 14)
+        assert_equal(test_case[:expected], signal_thing.run, "#{test_case[:data]} was wrong...")
       end
     end
   end
 end
 
-input_data = File.read(File.join(__dir__, "input.dat"))
-parts = %w(one two)
+input_data = File.read(File.join(__dir__, 'input.dat'))
+lengths = [4, 14]
 
-parts.each do |part|
-  puts ""
-  puts "Part #{part} Answer: #{SignalThing.new.run(input_data, problem_part: part)}"
-  puts ""
+lengths.each do |signal_length|
+  signal_thing = SignalThing.new(input_data:, signal_length:)
+  part = signal_length == 4 ? "One" : "Two"
+  puts "Part #{part} Answer: #{signal_thing.run}"
 end
