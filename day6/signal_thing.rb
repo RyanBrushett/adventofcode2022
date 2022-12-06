e361f932a9d8a7ceddb4198fedec0d97567e36d6
Ryan Brushett <ryanbrushett@gmail.com>
2022-12-06 00:29:15 -0500

    Day 6 Part 2

--- day6/signal_thing.rb
+++ day6/signal_thing.rb
@@ -1,10 +1,20 @@
 class SignalThing
   require 'minitest/autorun'
-  def run(data)
+
+  def run(data, problem_part:)
+    case problem_part
+    when "one"
+      signal_length = 4
+    when "two"
+      signal_length = 14
+    else
+      signal_length = "wat"
+    end
+
     chars = []
     char_count = 0
     data.each_char.with_index do |x, i|
-      if chars.length < 4
+      if chars.length < signal_length
         chars << x
         next
       end
@@ -14,7 +24,7 @@ class SignalThing
   end
 
   class SignalThingTest < Minitest::Test
-    def test_the_test_data
+    def test_the_test_data_for_part_one
       test_cases = [
         {data: "mjqjpqmgbljsphdztnvjfqwrcgsmlb", expected: 7},
         {data: "bvwbjplbgvbhsrlpgdmjqwftvncz", expected: 5},
@@ -25,7 +35,26 @@ class SignalThing
 
       test_cases.each do |test_case|
         assert_equal(
-          test_case[:expected], SignalThing.new.run(test_case[:data]),
+          test_case[:expected],
+          SignalThing.new.run(test_case[:data], problem_part: "one"),
+          "#{test_case[:data]} was wrong...",
+        )
+      end
+    end
+
+    def test_the_test_data_for_part_two
+      test_cases = [
+        {data: "mjqjpqmgbljsphdztnvjfqwrcgsmlb", expected: 19},
+        {data: "bvwbjplbgvbhsrlpgdmjqwftvncz", expected: 23},
+        {data: "nppdvjthqldpwncqszvftbrmjlhg", expected: 23},
+        {data: "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", expected: 29},
+        {data: "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", expected: 26},
+      ]
+
+      test_cases.each do |test_case|
+        assert_equal(
+          test_case[:expected],
+          SignalThing.new.run(test_case[:data], problem_part: "two"),
           "#{test_case[:data]} was wrong...",
         )
       end
@@ -33,6 +62,11 @@ class SignalThing
   end
 end
 
-puts ""
-puts "Answer: #{SignalThing.new.run(File.read(File.join(__dir__, "input.dat")))}"
-puts ""
+input_data = File.read(File.join(__dir__, "input.dat"))
+parts = %w(one two)
+
+parts.each do |part|
+  puts ""
+  puts "Part #{part} Answer: #{SignalThing.new.run(input_data, problem_part: part)}"
+  puts ""
+end
